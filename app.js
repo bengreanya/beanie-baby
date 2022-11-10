ce7835728b715d2bcb881bd8754e85e0bda833b3
Ben Greanya <bengreanya@gmail.com>
2022-11-09 18:10:34 -0800

    added submit and async function

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 /* Imports */
-import { getBabies, getTypes } from './fetch-utils.js';
-import { renderBabies, renderTypes } from './render-utils.js';
+import { getBabies, getAstro } from './fetch-utils.js';
+import { renderBabies, renderAstro } from './render-utils.js';
 /* Get DOM Elements */
 const babyList = document.getElementById('baby-list');
 const searchForm = document.getElementById('search-form');
-const typeSelect = document.getElementById('type-select');
+const astroSelect = document.getElementById('type-select');
 
 /* State */
 let babies = [];
 let astro = [];
 
 /* Events */
+// display all babies on page load
+window.addEventListener('load', async () => {
+    getBabies();
 
-/* Display Functions */
-
+    const astroResponse = await getAstro();
+    astro = astroResponse.data;
+    /* Display Functions */
+    // displayAstro();
+});
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(searchForm);
+    getBabies(formData.get('astroSign'));
+});
 // (don't forget to call any display functions you want to run on page load!)
