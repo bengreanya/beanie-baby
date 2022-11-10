22b6c77f46d9138047cb60856eaefeedd1417c03
Ben Greanya <bengreanya@gmail.com>
2022-11-09 17:42:41 -0800

    added imports and dom elements

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 /* Imports */
-
+import { getBabies, getTypes } from './fetch-utils.js';
+import { renderBabies, renderTypes } from './render-utils.js';
 /* Get DOM Elements */
+const babyList = document.getElementById('baby-list');
+const searchForm = document.getElementById('search-form');
+const typeSelect = document.getElementById('type-select');
 
 /* State */
+let babies = [];
+let astro = [];
 
 /* Events */
 
