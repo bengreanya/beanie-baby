4df009a3672349768c79e71a3ec87aa05d463af0
Harold Jean <haroldjean38@yahoo.com>
2022-11-09 20:34:15 -0500

    added fetch and render utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -5,3 +5,19 @@ const SUPABASE_KEY =
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // export async functions that fetch data
+export async function getBabies(BabyType) {
+    let query = client.from('beanie_babies').select('*').order('title').limit(100);
+
+    // if there is a Baby type given to the function, filter the response by type
+    if (BabyType) {
+        query = query.eq('Baby_Type', BabyType);
+    }
+    const response = await query;
+
+    return response;
+}
+
+export async function getTypes() {
+    const response = await client.from('Baby_types').select('*');
+    return response;
+}
